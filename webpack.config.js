42a91bddd7b1944c31f82625599ec9f92e46e2cd
haeun kim <ckhe1215@gmail.com>
2024-04-10 23:00:08 +0900

    :tada: Webpack, React, TypeScript 세팅

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+import path from "path";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __dirname = path.resolve();
+export default {
+  mode: "development",
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.min.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // 웹팩 실행시마다 dist 폴더 정리
+    new HtmlWebpackPlugin({
+      //index.html 자동 생성되도록 template 옵션 설정
+      template: "./src/index.html",
+    }),
+  ],
+};
